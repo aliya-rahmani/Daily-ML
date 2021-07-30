c268aaca7b5355fef9de5ccc1b1c8baf6c41bd7a
Aliya Rahmani <65673692+aliya-rahmani@users.noreply.github.com>
2021-07-30 21:11:24 +0530

    Update Shirt.py

--- Shirt.py
+++ Shirt.py
@@ -1,20 +1,20 @@
 # Use the Shirt Class
 
-You've seen what a class looks like and how to instantiate an object. Now it's your turn to write code that insantiates a shirt object.
+# You've seen what a class looks like and how to instantiate an object. Now it's your turn to write code that insantiates a shirt object.
 
 
 # Explanation of the Code
-This exercise using Jupyter notebook includes three files:
-- shirt_exercise.ipynb, which is the file you are currently looking at
-- answer.py containing answers to the exercise
-- tests.py, tests for checking your code - you can run these tests using the last code cell at the bottom of this notebook
+# This exercise using Jupyter notebook includes three files:
+# - shirt_exercise.ipynb, which is the file you are currently looking at
+# - answer.py containing answers to the exercise
+# - tests.py, tests for checking your code - you can run these tests using the last code cell at the bottom of this notebook
 
-# Your Task
-The shirt_exercise.ipynb file, which you are currently looking at if you are reading this, has an exercise to help guide you through coding with an object in Python.
 
-Fill out the TODOs in each section of the Jupyter notebook. You can find a solution in the answer.py file.
+# The shirt_exercise.ipynb file, which you are currently looking at if you are reading this, has an exercise to help guide you through coding with an object in Python.
 
-First, run this code cell below to load the Shirt class.
+# Fill out the TODOs in each section of the Jupyter notebook. You can find a solution in the answer.py file.
+
+# First, run this code cell below to load the Shirt class.
 
 class Shirt:
 
